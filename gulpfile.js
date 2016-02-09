38ca82b7c53166fcb03b08133a73dd2b69b9c4b8
MaZeS <erick@Ericks-MacBook-Air.local>
2016-02-09 09:48:33 +0100

    jshint+ package.json added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 
+var lint = require('gulp-jshint');
 var gulp = require('gulp');
 
 gulp.task('default', function() {
     //code for tasks
+});
+
+gulp.task('lint', function() {
+  return gulp.src('src/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('YOUR_REPORTER_HERE'));
 });
