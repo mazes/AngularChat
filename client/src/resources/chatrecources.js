451a576cb1ce97301e9670925f453496116957d7
moderndaymoron <ragnaringir@gmail.com>
2016-02-12 12:26:33 +0000

    resources

---
+++ client/src/resources/chatrecources.js
@@ -0,0 +1,19 @@
+"use strict";
+
+angular.module("angularChat").factory("ChatResource",
+function ChatResource(){
+	return {
+		login: function login(user, pass, callback){
+			//TOTO
+		},
+
+		getRoomList: function getRoomList(callback){
+			//TODO
+		},
+
+		getUsers: function getUsers(callback){
+
+		},
+		
+	}
+})
