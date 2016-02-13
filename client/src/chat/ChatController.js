e1c5a6302e6a509b866c456ce84fc1a1212dd241
MaZeS <erick@Ericks-MacBook-Air.local>
2016-02-13 21:25:11 +0100

    addded input to chat.html

--- client/src/chat/ChatController.js
+++ client/src/chat/ChatController.js
@@ -3,7 +3,19 @@
 angular.module("angularChat").controller("ChatController",
 ["$scope", "$http", "$location", "ChatResource",
 function ChatController($scope){
-	$scope.message = { ["hi" , "hello", "how are you?", "I'm great" ]
-	}
-
+	$scope.chat = [
+		{ user: "Jaun",
+		  timestamp: "21:31:06",
+		  message: "hi"},
+	    { user: "alexa",
+		  timestamp: "21:32:07",
+		  message: "hello"},
+		{ user: "Jaun",
+		  timestamp: "21:33:02",
+		  message: "how you doin?"},
+	    { user: "Jaun",
+	      timestamp: "21:35:20",
+		  message: "like a glove"},
+];
+	$scope.currentChatroom = "Funderland"
 }]);
