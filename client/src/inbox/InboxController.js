33919178a31aa982a19c8ca6f8dfeccbde0b0df3
Árni Oddsson <arniodds@gmail.com>
2016-02-19 15:42:41 +0000

    a

--- client/src/inbox/InboxController.js
+++ client/src/inbox/InboxController.js
@@ -3,42 +3,42 @@
 angular.module("angularChat").controller("InboxController",
 ["$scope","$routeParams", "$http", "$location", "ChatResource", "socket", "Notification",
 function InboxController($scope, $routeParams, $http, $location, ChatResource, socket, Notification){
-	$scope.currentUser = ChatResource.getUser();
-	$scope.unReadMessages = ChatResource.getNumberOfUnreadMessages();
-	
-	socket.on("recv_privatemsg", function(user, message){
-		ChatResource.addpMessage(message, user, $scope.currentUser);
-		$scope.newmessage = ChatResource.getNewestPmessage();
-		if($scope.newmessage.receiver === $scope.currentUser){
-			Notification.primary({
-				message: "You've received a private message from " + $scope.newmessage.sender,
-				templateUrl: "chat/notify.html",
-				scope: $scope,
-				delay: 7000
-			});
-			$scope.unReadMessages = ChatResource.getNumberOfUnreadMessages();
-			$scope.messages = $scope.getUnread();
-		}
-	});
+    $scope.currentUser = ChatResource.getUser();
+    $scope.unReadMessages = ChatResource.getNumberOfUnreadMessages();
+    
+    socket.on("recv_privatemsg", function(user, message){
+        ChatResource.addpMessage(message, user, $scope.currentUser);
+        $scope.newmessage = ChatResource.getNewestPmessage();
+        if($scope.newmessage.receiver === $scope.currentUser){
+            Notification.primary({
+                message: "You've received a private message from " + $scope.newmessage.sender,
+                templateUrl: "chat/notify.html",
+                scope: $scope,
+                delay: 7000
+            });
+            $scope.unReadMessages = ChatResource.getNumberOfUnreadMessages();
+            $scope.messages = $scope.getUnread();
+        }
+    });
 
-	$scope.getUnread = function getUnread(){
-		var messages = ChatResource.getpMessages();
-		var unread = [];
-		for(var i = 0; i < messages.length; i++){
-			if(!messages[i].read){
-				unread.push(messages[i]);
-			}
-		}
-		return unread;
-	};
+    $scope.getUnread = function getUnread(){
+        var messages = ChatResource.getpMessages();
+        var unread = [];
+        for(var i = 0; i < messages.length; i++){
+            if(!messages[i].read){
+                unread.push(messages[i]);
+            }
+        }
+        return unread;
+    };
 
-	$scope.gotoPm = function gotoPm(sender){
-		$location.url('/chat/private/' + sender);
-	};
+    $scope.gotoPm = function gotoPm(sender){
+        $location.url('/chat/private/' + sender);
+    };
 
-	$scope.messages = $scope.getUnread();
+    $scope.messages = $scope.getUnread();
 
-	$scope.$on("$destroy", function(){
-		socket.off("recv_privatemsg", function(success){});
-	});
+    $scope.$on("$destroy", function(){
+        socket.off("recv_privatemsg", function(success){});
+    });
 }]);
